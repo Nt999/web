8ba7ca7bfdd793a9645e0497ba55fc650a0e8279
Nt999 <hieu-nhan.tran@gmx.de>
2021-11-19 14:46:24 +0100

    debug step 5

--- p1/webteams/app/view.py
+++ p1/webteams/app/view.py
@@ -12,15 +12,24 @@ class View_cl(object):
    #-------------------------------------------------------
    def __init__(self):
    #-------------------------------------------------------
-      pass
+      self.lookup_o = TemplateLookup('./templates')
 
    #-------------------------------------------------------
    def createList_px(self, data_opl):
    #-------------------------------------------------------
-      template_s = self.readFile_p('list.tpl.html')
-      template_o = Templite(template_s)
-      markup_s = template_o.render(data_o = data_opl)
-      return markup_s
+        if (listform == 'tabelle'):
+            template_o = self.lookup_o.get_template('list.tpl')
+            listformNow = "tabelle"
+            listformNew = "liste"               #Für die umschalter
+            listformNewText = "Liste"
+        elif (listform == 'liste'):
+            template_o = self.lookup_o.get_template('list2.tpl')
+            listformNow = "liste"
+            listformNew = "tabelle"
+            listformNewText = "Tabelle"
+
+        markup_s = template_o.render(data_o = data_opl, listformNow = listformNow, listformNew = listformNew, listformNewText = listformNewText)
+        return markup_s
 
    #-------------------------------------------------------
    def createForm_px(self, id_spl, data_opl):
